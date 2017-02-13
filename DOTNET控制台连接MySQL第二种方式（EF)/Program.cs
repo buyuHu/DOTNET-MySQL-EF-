626701de7fc0aa89cde6f8c982086be180d09ba0
王国平 <@WANG>
2017-02-13 23:48:44 +0800

    添加项目文件。

---
+++ DOTNET控制台连接MySQL第二种方式（EF)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace DOTNET控制台连接MySQL第二种方式_EF_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new schoolEntities();
+            var text = new test();
+            text.id = 2;
+            text.name = "fang";
+            db.test.Add(text);
+            db.SaveChanges();
+            var table = from a in db.test
+                        where a.id == 1
+                        select a;
+            Console.WriteLine(table.FirstOrDefault().id);
+
+        }
+    }
+}
